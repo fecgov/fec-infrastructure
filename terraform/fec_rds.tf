b40a02403e1337454888d78b3bd7518b59523fd5
Joshua Carp <jm.carp@gmail.com>
2017-02-13 11:14:46 -0500

    Add concourse pipeline.
    Pairing with @ccostino

--- terraform/fec_rds.tf
+++ terraform/fec_rds.tf
@@ -41,21 +41,22 @@ resource "aws_db_subnet_group" "rds" {
   }
 }
 
+/* TODO: Lock down ingress rules */
 resource "aws_security_group" "rds" {
   name = "fec_rds"
 
   ingress {
-      from_port = 0
-      to_port = 0
-      protocol = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-      from_port = 0
-      to_port = 0
-      protocol = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
