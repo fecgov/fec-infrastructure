397c9a265eafd40604b73a95858c3a35e278e35e
LindsayYoung <lindsayn.young@gsa.gov>
2017-08-09 15:13:41 -0400

    Fixing the policy and assume role policy which had been switched before

--- terraform/fec_rds.tf
+++ terraform/fec_rds.tf
@@ -22,26 +22,6 @@ resource "aws_iam_role_policy" "test_policy" {
   role = "${aws_iam_role.test_role.id}"
 
   policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_role" "test_role" {
-  name = "test_role"
-
-  assume_role_policy = <<EOF
   {
     "Version": "2012-10-17",
       "Statement": [
@@ -70,6 +50,26 @@ resource "aws_iam_role" "test_role" {
               ]
           }
       ]
+  }
+EOF
+}
+
+resource "aws_iam_role" "test_role" {
+  name = "test_role"
+
+  assume_role_policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": "sts:AssumeRole",
+        "Principal": {
+          "Service": "ec2.amazonaws.com"
+        },
+        "Effect": "Allow",
+        "Sid": ""
+      }
+    ]
   }
   EOF
 }
